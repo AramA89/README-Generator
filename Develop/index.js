e3f19af0cecfbe9bb9648b2ca78160cc48446510
Aram Ambartsumyan <aram@Arams-MacBook-Air.local>
2022-10-18 17:40:28 -0700

    changes

--- Develop/index.js
+++ Develop/index.js
@@ -1,134 +1,72 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const {writeFile} = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = () => {
-    return inquirer.questions([
+    return inquirer.prompt ([
         {
             type: "input",
             name: "title",
             message: "Enter Project Title",
-            validate: (nameInput) => {
-                if (nameInput) {
-                    return true;
-                }   else {
-                    console.log("Please Enter A Project Title!");
-                    return false;
-                }
-            },
+           
         },
         
         {
             type: "input",
             name: "description",
-            message: "Provide Project A Description",
-            validate: (nameInput) => {
-                if (nameInput) {
-                    return true;
-                }   else {
-                    console.log("Please Provide A Description For Your Project!");
-                    return false;
-                }
-            },
+            message: "Provide Project Description",
+            
         },
 
         {
             type: "input",
             name: "installation",
             message: "Enter Project Installation Instructions",
-            validate: (nameInput) => {
-                if (nameInput) {
-                    return true;
-                }   else {
-                    console.log("Please Enter A Project Installation Instructions!");
-                    return false;
-                }
-            },
+            
         },
 
         {
             type: "input",
             name: "usage",
             message: "Enter Project Usage Information",
-            validate: (nameInput) => {
-                if (nameInput) {
-                    return true;
-                }   else {
-                    console.log("Please Enter Project Usage Information!");
-                    return false;
-                }
-            },
+           
         },
 
         {
             type: "input",
             name: "contribution",
             message: "Enter Project Contribution Guidlines",
-            validate: (nameInput) => {
-                if (nameInput) {
-                    return true;
-                }   else {
-                    console.log("Please Enter Project Contribution Guidlines!");
-                    return false;
-                }
-            },
+            
         },
 
         {
             type: "input",
             name: "test",
             message: "Enter Project Test Instructions",
-            validate: (nameInput) => {
-                if (nameInput) {
-                    return true;
-                }   else {
-                    console.log("Please Enter Project Test Instructions!");
-                    return false;
-                }
-            },
+            
         },
 
         {
             type: "input",
             name: "mockupimg",
-            message: "Attach A Project MockUp Image '<img src=''>' ",
-            validate: (nameInput) => {
-                if (nameInput) {
-                    return true;
-                }   else {
-                    console.log("Please Attach Project MockUp Image!");
-                    return false;
-                }
-            },
+            message: "Attach A Project MockUp Image Using This Format '<img src=''>' ",
+           
         },
 
         {
             type: "input",
             name: "deployedURL",
             message: "Enter Deployed Project URL",
-            validate: (nameInput) => {
-                if (nameInput) {
-                    return true;
-                }   else {
-                    console.log("Please Deployed Project URL!");
-                    return false;
-                }
-            },
+            
         },
 
         {
             type: "input",
             name: "github",
             message: "Enter GitHub Repository",
-            validate: (nameInput) => {
-                if (nameInput) {
-                    return true;
-                }   else {
-                    console.log("Please GitHub Repository!");
-                    return false;
-                }
-            },
+            
         },
 
         {
@@ -136,31 +74,25 @@ const questions = () => {
             name: "badge",
             message: "Select A Project License Badge",
             choices: [
-                "",
-                "",
-                "",
-            ]
-            validate: (nameInput) => {
-                if (nameInput) {
-                    return true;
-                }   else {
-                    console.log("Please Select A Project License Badge!");
-                    return false;
-                }
-            },
+                "Boost",
+                "IBM",
+                "MIT",
+                "Apache",
+            ],
+           
         },
 
-    ])
-}
+    ]);
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
     questions().then((data) => {
-        writeToFile(data);
+        writeFile(data);
     });
+    
 }
 
 // Function call to initialize app
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,81 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  const badge = {
+    Boost: '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)]',
+    IBM: '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)]',
+    MIT: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]',
+    Apache: '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]'
+  }
+  return badge[license];
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  const link = {
+    Boost: '(https://www.boost.org/LICENSE_1_0.txt)',
+    IBM: '(https://opensource.org/licenses/IPL-1.0)',
+    MIT: '(https://opensource.org/licenses/MIT)',
+    Apache: '(https://opensource.org/licenses/Apache-2.0)'
+  }
+  return link[license];
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license = '') {
+    return  ('Get license details at' + renderLicenseLink(license))
+  }
+  else {
+    return license;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
+
+  # ${renderLicenseBadge(data.badge)}
+  # ${renderLicenseLink(data.link)}
+  # ${renderLicenseSection}
+
+  ## Table Of Contents
+  * [Project Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contribution](#contribution)
+  * [Test](#test)
+  * [Mock Up](#mockupimg)
+  * [Deployed URL](#deployedURL)
+  * [GitHub Repository](#github)
+  
+  ## Description
+  # ${data.description}
+
+  ## Installation
+  # ${data.installation}
+
+  ## Usage
+  # ${data.usage}
+
+  ## Contribution
+  # ${data.conrtibution}
+
+  ## Test
+  # ${data.test}
+
+  ## Mock Up
+  # ${data.mockupimg}
+
+  ## Deployed URL
+  # ${data.deployedURL}
+
+  ## GitHub Repository
+  # ${data.github}
+
 
 `;
 }
